4a3635939ec28a67fff48e4638bbb415cc05c2cc
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2023-07-23 10:46:56 +0900

    スコア加算の修正

--- components/top/page.tsx
+++ components/top/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 import { Word } from "@/types/word";
 
@@ -20,9 +20,14 @@ export default function Home() {
   const [currentWord, setCurrentWord] = useState<Word | null>(null);
   const [typedWord, setTypedWord] = useState(""); // ユーザーがタイプしたワードを追跡
   const [successStreak, setSuccessStreak] = useState(0); // 連続成功回数を追跡
-  const [countdown, setCountdown] = useState(3); // カウントダウンの初期値を3に設定
   const wordTimer = useRef<NodeJS.Timeout | null>(null);
-  const countdownTimer = useRef<NodeJS.Timeout | null>(null); // カウントダウンタイマーの参照を追加
+  const wordStartTime = useRef<Date | null>(null); // ワード開始時間を追跡
+
+  const startAudio = useMemo(() => {
+    const audio = new Audio("/start.mp3");
+    audio.volume = 0.4; // 50% volume
+    return audio;
+  }, []);
 
   const gameAudio = useMemo(() => {
     const audio = new Audio("/game.mp3");
@@ -62,142 +67,31 @@ export default function Home() {
 
   const resultAudio = useMemo(() => {
     const audio = new Audio("/result.mp3");
-    audio.volume = 0.9; // 50% volume
-    return audio;
-  }, []);
-
-  const countdownAudio = useMemo(() => {
-    const audio = new Audio("/countdown.mp3");
     audio.volume = 0.5; // 50% volume
     return audio;
   }, []);
 
   useEffect(() => {
-    if (screen === "level" && gameStarted) {
+    if (screen === "start") {
+      startAudio.currentTime = 0; // 音楽が切り替わる場合、必ず最初から再生されるようにする
+      startAudio.play();
+      gameAudio.pause();
+    } else if (screen === "level" && gameStarted) {
       gameAudio.currentTime = 0; // 音楽が切り替わる場合、必ず最初から再生されるようにする
       gameAudio.play();
     } else if (screen === "home" || screen === "result") {
+      startAudio.pause();
       gameAudio.pause();
     }
-  }, [gameAudio, screen, gameStarted]);
+  }, [gameAudio, screen, startAudio, gameStarted]);
 
   useEffect(() => {
     const handleKeyDown = (event: { code: string }) => {
       if (event.code === "Space" && screen === "level") {
+        // スペースキーからエンターキーに変更
         setGameStarted(true);
-        countdownAudio.play(); // カウントダウン音を再生
-        countdownTimer.current = setInterval(() => {
-          setCountdown((prevCountdown) => prevCountdown - 1);
-        }, 1000); // 1秒ごとにカウントダウンを進行
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [countdownAudio, difficulty, screen]);
-
-  // カウントダウンが0になったらゲームを開始
-  useEffect(() => {
-    if (countdown === 0) {
-      setGameInProgress(true);
-      clearInterval(countdownTimer.current);
-      // Select a new word based on the difficulty
-      let wordList: string | any[];
-      switch (difficulty) {
-        case "easy":
-          wordList = easyWords;
-          break;
-        case "normal":
-          wordList = normalWords;
-          break;
-        case "hard":
-          wordList = hardWords;
-          break;
-        case "oni":
-          wordList = oniWords;
-          break;
-        default:
-          wordList = [];
-      }
-      const newWord = wordList[Math.floor(Math.random() * wordList.length)];
-      setCurrentWord(newWord);
-    }
-  }, [countdown, difficulty]);
-
-  // ユーザーがキーボードで文字を入力したときの処理
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (gameInProgress) {
-        const nextTypedWord = typedWord + event.key;
-        if (currentWord?.romaji.startsWith(nextTypedWord)) {
-          typeAudio.play(); // Play typing sound
-          setTypedWord(nextTypedWord);
-          setSuccessStreak((prevStreak) => prevStreak + 1); // Increment success streak
-          if (successStreak >= 10) {
-            bonusAudio.play(); // Play bonus sound
-            setTimer((prevTimer) => prevTimer + 1); // Add bonus time
-            setSuccessStreak(0); // Reset success streak
-          }
-          if (nextTypedWord === currentWord?.romaji) {
-            successAudio.play(); // Play success sound
-            setScore((prevScore) => prevScore + 1);
-            setTypedWord("");
-            // Select a new word based on the difficulty
-            let wordList: string | any[];
-            switch (difficulty) {
-              case "easy":
-                wordList = easyWords;
-                break;
-              case "normal":
-                wordList = normalWords;
-                break;
-              case "hard":
-                wordList = hardWords;
-                break;
-              case "oni":
-                wordList = oniWords;
-                break;
-              default:
-                wordList = [];
-            }
-            const newWord =
-              wordList[Math.floor(Math.random() * wordList.length)];
-            setCurrentWord(newWord);
-          }
-        } else {
-          missAudio.play(); // Play miss sound
-          setTimer((prevTimer) => Math.max(prevTimer - 1, 0)); // Subtract time for miss
-          setSuccessStreak(0); // Reset success streak
-        }
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [
-    gameInProgress,
-    currentWord,
-    typedWord,
-    difficulty,
-    typeAudio,
-    successAudio,
-    missAudio,
-    bonusAudio,
-    successStreak,
-  ]);
-
-  // 5秒間で1ワードの全ての文字をキーボード入力できなかったら、public/failure.mp3を流して次のワードを出す
-  useEffect(() => {
-    if (gameInProgress) {
-      wordTimer.current = setTimeout(() => {
-        failureAudio.play();
-        setTypedWord("");
+        setGameInProgress(true);
+        startAudio.play(); // エンターキーをクリックした後にstart.mp3を流す
         // Select a new word based on the difficulty
         let wordList: string | any[];
         switch (difficulty) {
@@ -218,15 +112,134 @@ export default function Home() {
         }
         const newWord = wordList[Math.floor(Math.random() * wordList.length)];
         setCurrentWord(newWord);
-      }, 5000);
-    }
+        wordStartTime.current = new Date(); // ワード開始時間を記録
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
 
     return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [difficulty, screen, startAudio]);
+
+  // 新しいワードを選択する関数
+  const selectNewWord = useCallback(() => {
+    let wordList: string | any[];
+    switch (difficulty) {
+      case "easy":
+        wordList = easyWords;
+        break;
+      case "normal":
+        wordList = normalWords;
+        break;
+      case "hard":
+        wordList = hardWords;
+        break;
+      case "oni":
+        wordList = oniWords;
+        break;
+      default:
+        wordList = [];
+    }
+    const newWord = wordList[Math.floor(Math.random() * wordList.length)];
+    setCurrentWord(newWord);
+    wordStartTime.current = new Date(); // ワード開始時間を記録
+  }, [difficulty]);
+
+  // ユーザーがキーボードで文字を入力したときの処理
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (gameInProgress && event.key.length === 1) {
+        const newTypedWord = typedWord + event.key;
+        if (currentWord?.romaji.startsWith(newTypedWord)) {
+          setTypedWord(newTypedWord);
+          typeAudio.play(); // キーボード入力時の音声を再生
+
+          if (newTypedWord === currentWord?.romaji) {
+            // ユーザーが現在のワードを全て入力した場合
+            successAudio.play(); // 全て一致したときの音声を再生
+            setScore((prevScore) => prevScore + 1); // スコアを更新
+            setTypedWord(""); // 入力された文字をリセット
+            selectNewWord(); // 新しいワードを選択
+          }
+        } else {
+          setTimer((prevTimer) => prevTimer - 1); // タイマーを1秒減らす
+          missAudio.play(); // 間違いの音声を再生
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [
+    gameInProgress,
+    currentWord,
+    typedWord,
+    selectNewWord,
+    typeAudio,
+    successAudio,
+    missAudio,
+  ]);
+
+  // 5秒間で1ワードの全ての文字をキーボード入力できなかったら、public/failure.mp3を流して次のワードを出す
+  useEffect(() => {
+    if (gameInProgress) {
       if (wordTimer.current) {
         clearTimeout(wordTimer.current);
       }
+
+      wordTimer.current = setTimeout(() => {
+        const now = new Date();
+        if (
+          wordStartTime.current &&
+          now.getTime() - wordStartTime.current.getTime() >= 5000
+        ) {
+          // 5秒以上経過していたら
+          failureAudio.play();
+          setScore((prevScore) => prevScore - 1); // スコアを1点減算
+          setTypedWord("");
+          selectNewWord();
+        }
+      }, 5000 - (Date.now() - (wordStartTime.current?.getTime() || 0))); // ここを修正
+
+      return () => {
+        if (wordTimer.current) {
+          clearTimeout(wordTimer.current);
+        }
+      };
+    }
+  }, [
+    gameInProgress,
+    difficulty,
+    failureAudio,
+    typedWord,
+    currentWord,
+    selectNewWord,
+  ]);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (gameInProgress && event.key.length === 1) {
+        setTypedWord((prevTypedWord) => prevTypedWord + event.key);
+        if (typedWord.length + 1 === currentWord?.romaji.length) {
+          // ユーザーが現在のワードを全て入力した場合
+          setScore((prevScore) => prevScore + 1); // スコアを更新
+          setTypedWord(""); // 入力された文字をリセット
+          selectNewWord(); // 新しいワードを選択
+        }
+      }
     };
-  }, [gameInProgress, difficulty, failureAudio]);
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [gameInProgress, currentWord, typedWord, selectNewWord]);
 
   const HomeScreen = () => (
     <div className="flex justify-center">
@@ -295,7 +308,7 @@ export default function Home() {
   );
 
   const LevelScreen = () => (
-    <div className="h-full flex flex-col p-4">
+    <div className="h-full flex flex-col items-center justify-center p-4">
       {gameInProgress && (
         <div className="mx-auto my-8 text-center">
           <p>{currentWord?.furigana}</p>
@@ -304,7 +317,12 @@ export default function Home() {
             {currentWord?.romaji.split("").map((char, index) => (
               <span
                 key={index}
-                style={{ color: typedWord.length > index ? "red" : "black" }}
+                style={{
+                  color:
+                    typedWord.length > index && typedWord[index] === char
+                      ? "red"
+                      : "black",
+                }}
               >
                 {char}
               </span>
@@ -315,27 +333,29 @@ export default function Home() {
       {!gameInProgress && screen === "level" && (
         <p className="flex justify-center">スペースキーを押してスタート</p>
       )}
-      {gameInProgress && <p className="mt-auto">Score: {score}</p>}
-      <p className="mt-auto">残り: {timer}秒</p>
+      {gameInProgress && (
+        <div className="">
+          <p className="mt-auto">Score: {score}</p>
+          <p className="mt-auto">残り: {timer}秒</p>
+        </div>
+      )}
     </div>
   );
 
   const ResultScreen = () => (
-    <div className="h-full flex flex-col p-4">
-      <div className="mx-auto my-8 text-center">
-        <p>ゲーム終了！</p>
-        <p className="text-2xl font-semibold">スコア: {score}</p>
-        <button
-          onClick={() => {
-            // SNSシェア機能を追加
-            const url = `https://twitter.com/intent/tweet?text=私のスコアは${score}点でした！&url=${window.location.href}&hashtags=タイピングゲーム`;
-            window.open(url, "_blank");
-          }}
-          className="bg-twitter text-white px-4 py-2 rounded-md mt-4"
-        >
-          Twitterでシェア
-        </button>
-      </div>
+    <div className="h-full flex flex-col justify-center items-center p-4">
+      <p>ゲーム終了！</p>
+      <p className="text-2xl font-semibold">スコア: {score}</p>
+      <button
+        onClick={() => {
+          // SNSシェア機能を追加
+          const url = `https://twitter.com/intent/tweet?text=私のスコアは${score}点でした！&url=${window.location.href}&hashtags=タイピングゲーム`;
+          window.open(url, "_blank");
+        }}
+        className="bg-twitter text-white px-4 py-2 rounded-md mt-4"
+      >
+        Twitterでシェア
+      </button>
     </div>
   );
 
