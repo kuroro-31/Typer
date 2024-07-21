8485b2aa80ccd5639fd95ca027e1c9993dbd745a
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2024-07-21 13:21:36 +0900

    ワード追加

--- components/top/words/level_1.ts
+++ components/top/words/level_1.ts
@@ -1,13 +1,136 @@
 import { Word } from "@/types/word";
 
 const level1: Word[] = [
-  { kanji: "慈愛", furigana: "じあい", romaji: ["jiai", "ziai"] },
   {
-    kanji: "シェフ",
-    furigana: "しえふ",
-    romaji: ["shefu", "shehu", "syefu", "syehu"],
+    kanji: "麒麟児",
+    furigana: "きりんじ",
+    romaji: ["kirinji", "kirinnji", "kirinzi", "kirinnzi"],
   },
-  { kanji: "賞", furigana: "しょう", romaji: ["shou", "syou"] },
+  { kanji: "外連味", furigana: "けれんみ", romaji: ["kerenmi", "kerennmi"] },
+  {
+    kanji: "真善美",
+    furigana: "しんぜんび",
+    romaji: [
+      "sinzenbi",
+      "sinzennbi",
+      "sinnzennbi",
+      "shinzenbi",
+      "shinzennbi",
+      "shinnzennbi",
+    ],
+  },
+  { kanji: "千里眼", furigana: "せんりがん", romaji: ["senrigann"] },
+  {
+    kanji: "天地人",
+    furigana: "てんちじん",
+    romaji: ["tenchijinn", "tenchizinn", "tentijinn", "tentizinn"],
+  },
+  { kanji: "登竜門", furigana: "とうりゅうもん", romaji: ["touryuumonn"] },
+  {
+    kanji: "伏魔殿",
+    furigana: "ふくまでん",
+    romaji: ["fukumadenn", "hukumadenn"],
+  },
+  {
+    kanji: "不世出",
+    furigana: "ふせいしゅつ",
+    romaji: ["fuseishutsu", "huseisyutsu", "huseishutu"],
+  },
+  {
+    kanji: "不退転",
+    furigana: "ふたいてん",
+    romaji: ["futaitenn", "hutaitenn"],
+  },
+  { kanji: "色眼鏡", furigana: "いろめがね", romaji: ["iromegane"] },
+  {
+    kanji: "有頂天",
+    furigana: "うちょうてん",
+    romaji: ["uchoutenn", "utyoutenn"],
+  },
+  { kanji: "大御所", furigana: "おおごしょ", romaji: ["oogosho", "oogosyo"] },
+  { kanji: "十八番", furigana: "おはこ", romaji: ["ohako"] },
+  {
+    kanji: "金字塔",
+    furigana: "きんじとう",
+    romaji: ["kinjitou", "kinzitou", "kinnjitou", "kinnzitou"],
+  },
+  { kanji: "下馬評", furigana: "げばひょう", romaji: ["gebahyou"] },
+  { kanji: "紅一点", furigana: "こういってん", romaji: ["kouitten"] },
+  {
+    kanji: "金輪際",
+    furigana: "こんりんざい",
+    romaji: ["konrinzai", "konnrinzai"],
+  },
+  { kanji: "修羅場", furigana: "しゅらば", romaji: ["shuraba", "syuraba"] },
+  { kanji: "太鼓判", furigana: "たいこばん", romaji: ["taikoban"] },
+  {
+    kanji: "天王山",
+    furigana: "てんのうざん",
+    romaji: ["tennouzan", "tennnouzan"],
+  },
+  { kanji: "檜舞台", furigana: "ひのきぶたい", romaji: ["hinokibutai"] },
+  {
+    kanji: "風物詩",
+    furigana: "ふうぶつし",
+    romaji: [
+      "fuubutsushi",
+      "fuubutsusi",
+      "fubutushi",
+      "huubutsushi",
+      "huubutsusi",
+      "huubutushi",
+    ],
+  },
+  {
+    kanji: "胸算用",
+    furigana: "むなざんよう",
+    romaji: ["munazanyou", "munazannyou"],
+  },
+  {
+    kanji: "天邪鬼",
+    furigana: "あまのじゃく",
+    romaji: ["amanojaku", "amanozyaku"],
+  },
+  {
+    kanji: "安本丹",
+    furigana: "あんぽんたん",
+    romaji: ["anpontan", "annpontan"],
+  },
+  { kanji: "兎兵法", furigana: "うさぎへいほう", romaji: ["usagiheihou"] },
+  {
+    kanji: "社用族",
+    furigana: "しゃようぞく",
+    romaji: ["shayouzoku", "syayouzoku"],
+  },
+  {
+    kanji: "素寒貧",
+    furigana: "すかんぴん",
+    romaji: ["sukanpin", "sukannpin"],
+  },
+  { kanji: "素頓狂", furigana: "すっとんきょう", romaji: ["suttonkyou"] },
+  { kanji: "太平楽", furigana: "たいへいらく", romaji: ["taiheiraku"] },
+  { kanji: "棚牡丹", furigana: "たなぼた", romaji: ["tanabota"] },
+  {
+    kanji: "珍紛漢",
+    furigana: "ちんぷんかん",
+    romaji: [
+      "chinpunkann",
+      "chinnpunkann",
+      "chinpunnkann",
+      "chinnpunnkann",
+      "tinpunkann",
+      "tinnpunkann",
+      "tinpunnkann",
+      "tinnpunnkann",
+    ],
+  },
+  { kanji: "出鱈目", furigana: "でたらめ", romaji: ["detarame"] },
+  {
+    kanji: "唐変木",
+    furigana: "とうへんぼく",
+    romaji: ["touhenboku", "touhennboku"],
+  },
+  // { kanji: "頓珍漢", furigana: "とんちんかん", romaji: ["tonsinkan"] },
 ];
 
 export default level1;
